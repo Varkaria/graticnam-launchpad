c631a6e6eba35d0267c062eed8a6c84c21332535
Pavaresorn Malai <61514399+Varkaria@users.noreply.github.com>
2022-07-04 23:25:32 +0700

    chore: update jerm

--- .monk/config.js
+++ .monk/config.js
@@ -1,8 +1,8 @@
 const config = {
   //Dangerous it's will replace all arabic numbers to thai numbers (0123456789 -> ๐๑๒๓๔๕๖๗๘๙)) may it make your code broken
-  thaiLover: true,
+  thaiLover: false,
   //Dangerous it's will add holy header to every unknown files and make your some file broken
-  veryHoly: true,
+  veryHoly: false,
   //Monk will ingore this (regex)
   ignore: [
       /^\.monk/,
